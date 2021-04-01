ce4b96066c6a72dcc6e2d5536b09a6626a8892f1
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-04-01 02:03:40 +0200

    Implemented navbar

--- festival-jeu/ContentView.swift
+++ festival-jeu/ContentView.swift
@@ -9,76 +9,60 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-    
     @State var results = JeuList()
+    @State var showMenu = false
 
     var body: some View {
-//        List {
-//            ForEach(items) { item in
-//                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//            }
-//            .onDelete(perform: deleteItems)
-//        }
-//        .toolbar {
-//            #if os(iOS)
-//            EditButton()
-//            #endif
-//
-//            Button(action: addItem) {
-//                Label("Add Item", systemImage: "plus")
-//            }
-//        }
-        SearchJeuListView(searchJeuList: SearchJeuListViewModel(JeuList()))
+        
+        let drag = DragGesture()
+            .onEnded{
+                if $0.translation.width < -100 {
+                    withAnimation{
+                        self.showMenu=false
+                    }
+                }
+            }
+        return NavigationView{
+            GeometryReader { geometry in
+                ZStack(alignment: .leading){
+                    MainView(showMenu: self.$showMenu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x:self.showMenu ? geometry.size.width/2:0)
+                        .disabled(self.showMenu ? true: false)
+                    if self.showMenu{
+                        MenuView()
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge:.leading))
+                    }
+                }.gesture(drag)
+            }
+            .navigationBarTitle("Side menu", displayMode: .inline)
+            .navigationBarItems(leading: (
+                Button(action: {
+                    withAnimation {
+                        self.showMenu.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                }
+            ))
+        }
         
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
+//        SearchJeuListView(searchJeuList: SearchJeuListViewModel(JeuList()))
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct MainView: View {
+    
+    @Binding var showMenu: Bool
+    var body: some View {
+        Button(action: {
+            withAnimation{
+                self.showMenu=true
+            }
+        }) {
+            Text("Show menu")
+        }
     }
 }
---
+++ festival-jeu/View/MenuView.swift
@@ -0,0 +1,62 @@
+//
+//  MenuView.swift
+//  festival-jeu
+//
+//  Created by ayoub moujane on 01/04/2021.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            HStack{
+                Image(systemName: "house")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Accueil")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top,100)
+            HStack{
+                Image(systemName: "gamecontroller")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Jeux")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top,30)
+            HStack{
+                Image(systemName: "location")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Zones")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top,30)
+            HStack{
+                Image(systemName: "person.crop.square")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Exposants")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top,30)
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
